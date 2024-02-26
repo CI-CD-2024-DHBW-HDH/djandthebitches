c6c4ca3ce4f16b0871dacb64dd848b076bd80584
adamickik <adamicki.karolina@gmail.com>
2024-02-26 13:56:47 +0100

    prettier fix

--- src/logic/game.test.ts
+++ src/logic/game.test.ts
@@ -137,19 +137,19 @@
 // });
 
 /* eslint-disable no-undef */
-import { Field, won } from './game'
+import { Field, won } from "./game";
 
-describe('won', () => {
-  it('player1 won', () => {
-    const result = won([1, 1, 1, 0, 0, 0, 0, 0, 0])
-    expect(result).toBe(Field.PLAYER1)
-  })
-  it('player2 won', () => {
-    const result = won([0, 0, 0, 2, 2, 2, 0, 0, 0])
-    expect(result).toBe(Field.PLAYER2)
-  })
-  it('draw', () => {
-    const result = won([0, 0, 0, 0, 0, 0, 0, 0, 0])
-    expect(result).toBe(Field.EMPTY)
-  })
-})
+describe("won", () => {
+  it("player1 won", () => {
+    const result = won([1, 1, 1, 0, 0, 0, 0, 0, 0]);
+    expect(result).toBe(Field.PLAYER1);
+  });
+  it("player2 won", () => {
+    const result = won([0, 0, 0, 2, 2, 2, 0, 0, 0]);
+    expect(result).toBe(Field.PLAYER2);
+  });
+  it("draw", () => {
+    const result = won([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    expect(result).toBe(Field.EMPTY);
+  });
+});
