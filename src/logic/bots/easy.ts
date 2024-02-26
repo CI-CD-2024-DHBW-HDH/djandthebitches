0123a3068592ffc29031baa6959e280e41b448c6
marco-maier3 <maierm.tin21@student.dhbw-heidenheim.de>
2024-02-26 10:33:35 +0100

    feat easyMove

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,7 +1,29 @@
-import { Field } from "../game";
+import { Field, won, getBlanks } from "../game";
+import { randomMove } from "./bot";
 
 // the easy bot plays a winning move, if it can
 // otherwise, it plays a random move
 export function easyMove(board: Field[], own: Field): number {
-  return -1
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === Field.EMPTY) {
+      // Simulate the move
+      const tempBoard = [...board];
+      tempBoard[i] = own;
+
+      // Check if this move wins the game
+      if (won(tempBoard) === own) {
+        return i; // Return the winning move
+      }
+    }
+  }
+  const emptySpots = getBlanks(board);
+  const bounds = emptySpots.length; // This is the number of available empty spots
+
+  if (bounds === 0) {
+    return -1; // No move is possible
+  }
+
+  const randomIndex = randomMove(bounds); // Get a random index based on the number of empty spots
+  return emptySpots[randomIndex]; // Return the actual board index of the random empty spot
 }
+
