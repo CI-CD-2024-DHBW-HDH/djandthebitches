14bda156c0c04c1c0e98dd525afe055939239329
Rösch, David - TIN2021 <roeschd.tin21@student.dhbw-heidenheim.de>
2024-02-26 13:54:54 +0100

    config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,23 @@
-export {};
+// export {};
 
-module.exports = {
+// module.exports = {
+//   testEnvironment: 'node',
+//   testMatch: ['tests/game.test.ts'],
+//   transform: {
+//     '^.+\\.ts$': 'ts-jest',
+//   },
+//   moduleFileExtensions: ['ts', 'js'],
+//   coverageDirectory: 'coverage',
+//   collectCoverageFrom: ['src/**/*.ts'],
+// };
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['tests/game.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.ts'],
-};
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}
+
+export default config
