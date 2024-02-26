28d0bd8a41adb8d7b2301617969f3971d2d83bc7
adamickik <adamicki.karolina@gmail.com>
2024-02-26 13:40:26 +0100

    prettier fix

--- src/logic/game.ts
+++ src/logic/game.ts
@@ -1,4 +1,4 @@
-import { moveWithMode, type BotMove } from './bots/bot'
+import { moveWithMode, type BotMove } from "./bots/bot";
 
 export enum Mode {
   EASY = 0,
@@ -16,96 +16,96 @@ export enum Field {
 }
 
 export class Player {
-  score = 0
-  field: Field.PLAYER1 | Field.PLAYER2
-  botMove: BotMove | undefined = undefined
+  score = 0;
+  field: Field.PLAYER1 | Field.PLAYER2;
+  botMove: BotMove | undefined = undefined;
 
-  constructor (f: Field.PLAYER1 | Field.PLAYER2) {
-    this.field = f
+  constructor(f: Field.PLAYER1 | Field.PLAYER2) {
+    this.field = f;
   }
 
-  addWin () {
-    this.score++
+  addWin() {
+    this.score++;
   }
 
-  isHuman (): boolean {
-    return this.botMove === undefined
+  isHuman(): boolean {
+    return this.botMove === undefined;
   }
 
-  move (board: Field[]): number {
-    if (this.botMove !== undefined) return this.botMove(board, this.field)
-    return -1
+  move(board: Field[]): number {
+    if (this.botMove !== undefined) return this.botMove(board, this.field);
+    return -1;
   }
 }
 
 export class Game {
-  player: Player
-  enemy: Player
-  mode: Mode
+  player: Player;
+  enemy: Player;
+  mode: Mode;
 
-  constructor (
+  constructor(
     player: Player = new Player(Field.PLAYER1),
     enemy: Player = new Player(Field.PLAYER2),
-    mode: Mode = Mode.EASY
+    mode: Mode = Mode.EASY,
   ) {
-    player.score = 0
-    enemy.score = 0
-    this.mode = mode
-    this.player = player
-    this.enemy = enemy
-    this.enemy.botMove = moveWithMode(this.mode)
+    player.score = 0;
+    enemy.score = 0;
+    this.mode = mode;
+    this.player = player;
+    this.enemy = enemy;
+    this.enemy.botMove = moveWithMode(this.mode);
   }
 
-  addWin (player: Field) {
+  addWin(player: Field) {
     switch (player) {
       case Field.PLAYER1:
-        this.player.addWin()
-        break
+        this.player.addWin();
+        break;
       case Field.PLAYER2:
-        this.enemy.addWin()
-        break
+        this.enemy.addWin();
+        break;
     }
   }
 
-  switchSides () {
-    const botMove = this.player.botMove
-    this.player.botMove = this.enemy.botMove
-    this.enemy.botMove = botMove
-    const score = this.player.score
-    this.player.score = this.enemy.score
-    this.enemy.score = score
+  switchSides() {
+    const botMove = this.player.botMove;
+    this.player.botMove = this.enemy.botMove;
+    this.enemy.botMove = botMove;
+    const score = this.player.score;
+    this.player.score = this.enemy.score;
+    this.enemy.score = score;
   }
 
-  updateMode (mode: Mode) {
-    this.mode = mode
+  updateMode(mode: Mode) {
+    this.mode = mode;
     if (this.player.isHuman() && this.enemy.isHuman() && mode != Mode.HUMAN) {
-      this.enemy.botMove = moveWithMode(this.mode)
-      return
+      this.enemy.botMove = moveWithMode(this.mode);
+      return;
     }
-    if (!this.player.isHuman()) this.player.botMove = moveWithMode(this.mode)
-    if (!this.enemy.isHuman()) this.enemy.botMove = moveWithMode(this.mode)
+    if (!this.player.isHuman()) this.player.botMove = moveWithMode(this.mode);
+    if (!this.enemy.isHuman()) this.enemy.botMove = moveWithMode(this.mode);
   }
 }
 
 export class Outcome {
-  finished: boolean
-  winner: Field = Field.EMPTY
+  finished: boolean;
+  winner: Field = Field.EMPTY;
 
-  constructor (board: Field[]) {
-    this.winner = won(board)
-    this.finished = isFull(board) || this.winner !== Field.EMPTY
+  constructor(board: Field[]) {
+    this.winner = won(board);
+    this.finished = isFull(board) || this.winner !== Field.EMPTY;
   }
 
-  isDraw (): boolean {
-    return this.finished && this.winner === Field.EMPTY
+  isDraw(): boolean {
+    return this.finished && this.winner === Field.EMPTY;
   }
 }
 
-export function isFull (board: Field[]): boolean {
-  return !board.some((field) => field === Field.EMPTY)
+export function isFull(board: Field[]): boolean {
+  return !board.some((field) => field === Field.EMPTY);
 }
 
-export function won (board: Field[]): Field {
+export function won(board: Field[]): Field {
   // Check rows
   for (let i = 0; i < 3; i++) {
     if (
@@ -113,7 +113,7 @@ export function won (board: Field[]): Field {
       board[i * 3] === board[i * 3 + 1] &&
       board[i * 3 + 1] === board[i * 3 + 2]
     ) {
-      return board[i * 3] // Player 1 or Player 2 wins
+      return board[i * 3]; // Player 1 or Player 2 wins
     }
   }
 
@@ -124,7 +124,7 @@ export function won (board: Field[]): Field {
       board[i] === board[i + 3] &&
       board[i + 3] === board[i + 6]
     ) {
-      return board[i] // Player 1 or Player 2 wins
+      return board[i]; // Player 1 or Player 2 wins
     }
   }
 
@@ -135,34 +135,34 @@ export function won (board: Field[]): Field {
       board[4] === board[8]) ||
     (board[2] !== Field.EMPTY && board[2] === board[4] && board[4] === board[6])
   ) {
-    return board[4] // Player 1 or Player 2 wins
+    return board[4]; // Player 1 or Player 2 wins
   }
 
-  return Field.EMPTY // No winner yet
+  return Field.EMPTY; // No winner yet
 }
 
-export function newBoard (): Field[] {
-  const board = new Array<Field>(9)
-  board.fill(Field.EMPTY)
-  return board
+export function newBoard(): Field[] {
+  const board = new Array<Field>(9);
+  board.fill(Field.EMPTY);
+  return board;
 }
 
-export function getBlanks (board: Field[]): number[] {
+export function getBlanks(board: Field[]): number[] {
   return board
     .map<number>((field, index) => {
-    if (field !== Field.EMPTY) return -1
-    return index
-  })
-    .filter((value) => {
-      return value >= 0
+      if (field !== Field.EMPTY) return -1;
+      return index;
     })
+    .filter((value) => {
+      return value >= 0;
+    });
 }
 
-export function invertPlayer (player: Field): Field {
-  if (!isPlayer(player)) return Field.EMPTY
-  return 3 - player
+export function invertPlayer(player: Field): Field {
+  if (!isPlayer(player)) return Field.EMPTY;
+  return 3 - player;
 }
 
-export function isPlayer (player: Field): boolean {
-  return player === Field.PLAYER1 || player === Field.PLAYER2
+export function isPlayer(player: Field): boolean {
+  return player === Field.PLAYER1 || player === Field.PLAYER2;
 }
